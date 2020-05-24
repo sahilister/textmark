0b9eee7404c787e8e3030d2a29c995b6afa41dd9
sahilister <sahil.dhiman27091999@gmail.com>
2020-05-24 05:35:19 +0530

    addeventlistener for live output

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ import turndownPluginGfm from './turndownPluginGfm.js'
 const gfm = turndownPluginGfm.gfm 
 const turndownService = new TurndownService({ headingStyle: 'atx'})
 turndownService.use(gfm)
-console.log("start text")
 
-let trixOutput =  document.getElementById('trix-output')
-let turndownOutput = document.getElementById('turndown-output')
-turndownOutput.innerHTML = turndownService.turndown(trixOutput.value)
-console.log(trixOutput.value)
+addEventListener('trix-change', () => {
+    let trixOutput =  document.getElementById('trix-output')
+    let turndownOutput = document.getElementById('turndown-output')
+    turndownOutput.innerHTML = turndownService.turndown(trixOutput.value)
+    console.log(trixOutput.value)
+})
+
 
 // turndownOutput.textContent = turndownService.turndown('<h2>hello world</h2>')
 //
