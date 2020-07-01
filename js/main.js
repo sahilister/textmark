f000a7d0ce8c4a72099e97db86e158ce43127d9e
sahilister <sahil.dhiman27091999@gmail.com>
2020-07-01 20:56:46 +0530

    add full quill editor

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
-import TurndownService from '../dist/turndown.min.js'
-import turndownPluginGfm from '../dist/turndownPluginGfm.min.js'
+import TurndownService from './vendor/turndown.min.js'
+import turndownPluginGfm from './vendor/turndownPluginGfm.min.js'
 const gfm = turndownPluginGfm.gfm
 const turndownService = new TurndownService({ headingStyle: 'atx'})
 turndownService.use(gfm)
 
-// addEventListener('trix-change', () => {
-//     let trixOutput =  document.getElementById('trix-output')
-//     let turndownOutput = document.getElementById('turndown-output')
-//     turndownOutput.innerHTML = turndownService.turndown(trixOutput.value)
-//     console.log(trixOutput.value)
-// })
 
 let toolbarOptions = [
   ['bold', 'italic', 'strike'],
   [{ 'header': 1 }, { 'header': 2 }],
-  ['code', 'blockquote', 'code-block'],
+  ['code', 'blockquote'],
   [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-   ['link', 'image']]
+  ['link']
+]
 
 let quill = new Quill('#editor', {
   modules: {
